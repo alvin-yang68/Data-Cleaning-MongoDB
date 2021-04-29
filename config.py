5bb55c11ff85df6dd43bd7b4c75802d79eddb004
alvin-yang68 <alvin.yang68@gmail.com>
2021-04-29 14:40:22 +1200

    Fix: Refactor hard coded connection string to Heroku config var

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    MONGO_URI = 'mongodb+srv://Alvin:R2y7sefqZJL9cXp0@cluster0.xhr3k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    MONGO_URI = os.environ.get('MONGODB_URI')
 
 # waitress-serve --listen=localhost:8000 nosqlcleaner:app
 # heroku ps:scale web=1
