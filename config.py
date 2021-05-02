7a05882925dd502e8a03d4933ce129ef193eeb0b
alvin-yang68 <alvin.yang68@gmail.com>
2021-05-02 15:06:44 +1200

    Append requirements.txt

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     MONGO_URI = os.environ.get(
-        'MONGODB_URI')
+        'MONGODB_URI') or 'mongodb+srv://Alvin:R2y7sefqZJL9cXp0@cluster0.xhr3k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
     NEO4J_URI = 'bolt://3.86.16.126:7687'
     NEO4J_USER = 'neo4j'
     NEO4J_PASSWORD = 'discriminations-jar-shocks'
